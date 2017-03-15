5c5f2e42f941a3d16a139de6805c36c51bd77ed0
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-03-15 19:53:10 +0700

    finish milestone 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,22 @@ class MessagesController < ApplicationController
     @messages = Message.all.order(id: :desc)
   end
 
+  def create
+    @message = Message.new message_params
+    if @message.save
+      flash[:success] = "Message created successfully"
+      redirect_to messages_path
+    else
+      render 'index'
+      flash.now[:error] = "Cannot create the message"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end 
+  
+
 end
